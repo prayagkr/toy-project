9d6e2072ca7aa927fd02cc16bbafacbe33dafdfd
Pramod Kumar Sharma <prayagkr@gmail.com>
2020-09-12 16:58:55 +0530

    added config ini and db file

---
+++ src/db.py
@@ -0,0 +1,15 @@
+
+import configparser
+import pymysql
+
+CP = configparser.ConfigParser()
+CP.read('config.ini')
+
+# sql db connection details
+DATABASE = CP.get('sql_conn', 'database')
+HOST = CP.get('sql_conn', 'host')
+USERNAME = CP.get('sql_conn', 'username')
+PASSWORD = CP.get('sql_conn', 'password')
+
+
+db = pymysql.connect(HOST, USERNAME, PASSWORD, DATABASE)
